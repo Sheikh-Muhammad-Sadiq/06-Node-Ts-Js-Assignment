36bdc09a2d62f54800491416372498b3c84af3df
Sheikh-Muhammad-Sadiq <162907621+Sheikh-Muhammad-Sadiq@users.noreply.github.com>
2024-03-17 03:06:43 -0700

    Add files via upload
    Q 06 Node.js of Assignments

---
+++ Q6.ts
@@ -0,0 +1,9 @@
+// 6. Stripping Names: Store a Person's name, and include some whitespace charecters at the beginning
+//  and end of the name. Make sure you use each
+//  character combination, "\t" and "\n", at least once. Print the name once, so The
+//  whitespace around the name is displayed. Then  Print the name after striping the white spaces.
+
+console.log("HelloMyWorld")
+console.log("Hello\tMy\tWorld")
+console.log("Hello\nMyWorld")
+console.log("My\tFather\tName\tIs\tInam\tUl\tHaq")
